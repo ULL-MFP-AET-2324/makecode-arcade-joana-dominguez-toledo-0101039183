8cdd51d6102982b66a90ae26c43330e228330e55
Joana <148484350+Joanadt98@users.noreply.github.com>
2023-11-07 18:34:14 +0000

    Actualizar pxt.json, main.blocks, main.ts, images.g.ts

--- main.ts
+++ main.ts
@@ -1,60 +1,38 @@
-sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
-    info.changeScoreBy(1)
-    hotdog.setPosition(randint(0, 160), randint(0, 120))
-    info.startCountdown(10)
-})
-let hotdog: Sprite = null
-scene.setBackgroundColor(10)
-let mySprite = sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . 3 3 3 3 . . . . . . 
-    . . 3 3 3 3 3 3 3 3 3 3 3 3 . . 
-    . . 3 3 3 3 f f f f 3 3 3 3 . . 
-    . . 3 3 3 3 f f f f 3 3 3 3 . . 
-    . . 3 3 3 3 f f f f 3 3 3 3 . . 
-    . . 3 3 3 3 3 3 3 3 3 3 3 3 . . 
-    . . . 3 3 3 . . . . 3 3 3 . . . 
-    . . . 3 3 3 . . . . 3 3 3 . . . 
-    . . . 3 3 3 . . . . 3 3 3 . . . 
-    . . . . . . 3 3 3 3 . . . . . . 
-    . . . . . . 3 3 3 3 . . . . . . 
-    . . . . . . 3 3 3 3 . . . . . . 
-    . . . . . . . . . . . . . . . . 
+scene.setBackgroundColor(3)
+let Person = sprites.create(img`
+    . . . . . f f f f . . . . . 
+    . . . f f 5 5 5 5 f f . . . 
+    . . f 5 5 5 5 5 5 5 5 f . . 
+    . f 5 5 5 5 5 5 5 5 5 5 f . 
+    . f 5 5 5 d b b d 5 5 5 f . 
+    f 5 5 5 b 4 4 4 4 b 5 5 5 f 
+    f 5 5 c c 4 4 4 4 c c 5 5 f 
+    f b b f b f 4 4 f b f b b f 
+    f b b 4 1 f d d f 1 4 b b f 
+    . f b f d d d d d d f b f . 
+    . f e f e 4 4 4 4 e f e f . 
+    . e 4 f 6 9 9 9 9 6 f 4 e . 
+    . 4 d c 9 9 9 9 9 9 c d 4 . 
+    . 4 f b 3 b 3 b 3 b b f 4 . 
+    . . f f 3 b 3 b 3 3 f f . . 
+    . . . . f f b b f f . . . . 
     `, SpriteKind.Player)
-controller.moveSprite(mySprite)
-hotdog = sprites.create(img`
-    ..............bbbbbbb...........
-    ...........bb66663333baa........
-    .........bb3367776333663aa......
-    ........b33333888333389633aa....
-    .......b3333333333333389633aa...
-    ......b34443333333333338633bae..
-    .....b3455433333333334443333ae..
-    ....b33322333dddd3333455233daee.
-    ...b3d333333dd3bbbb33322333dabe.
-    ..b3d333333d3bb33bb33333333da4e.
-    ..bd33333333b33aab3333333223a4ee
-    .b3d3663333b33aab33366332442b4ee
-    .bd3b983333a3aa3333387633ee3b4ee
-    .bd6983333baaa333333387633bb4bee
-    b3d6833333bba333333333863ba44ebe
-    bdd3333333bb3333333333333a44bebe
-    add666633333322333366333ba44bbbe
-    ad67776333332442336983d3a444b4e.
-    add888b333333ee3369833d3a44b44e.
-    add333333333333336833d3a444b4e..
-    a3dd3333344433333dddd3a444b44e..
-    ab33ddd325543333dd33aa444b44e...
-    .eabb3dd32233333baaa4444b44e....
-    .ebabb3d333d33baa444443b44e.....
-    ..ebaab3ddd3aaa4444433b44e......
-    ..eebbaab33a44444333b444e.......
-    ...eeebbaab444b333b4444e........
-    ....ebeeebbbbbbbb4444ee.........
-    .....eebbbb44444444ee...........
-    .......eeebbb444eee.............
-    ..........eeeeee................
-    ................................
+controller.moveSprite(Person)
+let comida = sprites.create(img`
+    . . . . . . . . . . . 6 6 6 6 6 
+    . . . . . . . . . 6 6 7 7 7 7 8 
+    . . . . . . 8 8 8 7 7 8 8 6 8 8 
+    . . e e e e c 6 6 8 8 . 8 7 8 . 
+    . e 2 5 4 2 e c 8 . . . 6 7 8 . 
+    e 2 4 2 2 2 2 2 c . . . 6 7 8 . 
+    e 2 2 2 2 2 2 2 c . . . 8 6 8 . 
+    e 2 e e 2 2 2 2 e e e e c 6 8 . 
+    c 2 e e 2 2 2 2 e 2 5 4 2 c 8 . 
+    . c 2 e e e 2 e 2 4 2 2 2 2 c . 
+    . . c 2 2 2 e e 2 2 2 2 2 2 2 e 
+    . . . e c c e c 2 2 2 2 2 2 2 e 
+    . . . . . . . c 2 e e 2 2 e 2 c 
+    . . . . . . . c e e e e e e 2 c 
+    . . . . . . . . c e 2 2 2 2 c . 
+    . . . . . . . . . c c c c c . . 
     `, SpriteKind.Food)
